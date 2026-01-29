f0662d43acde4607909e38bfcb4c91c5ced35115
Nemmer772004 <namnamnamaa8l@gmail.com>
2026-01-29 09:08:23 +0700

    nemmer su l sai backup va sua ;ai tuong toang roi

--- addons/quan_ly_tai_san/__manifest__.py
+++ addons/quan_ly_tai_san/__manifest__.py
@@ -13,7 +13,6 @@
         - Chuyển giao tài sản giữa phòng ban/nhân viên
         - Kiểm kê tài sản định kỳ
         - Tính toán khấu hao tự động (3 phương pháp)
-        - Dashboard tổng quan và phân tích
         - Mã QR code cho tài sản
         - Liên kết với module Nhân sự và Phòng họp
     """,
@@ -26,7 +25,7 @@
     'license': 'LGPL-3',
 
     # Module phụ thuộc
-    'depends': ['base', 'mail', 'quan_ly_nhan_su', 'web'],
+    'depends': ['base', 'mail', 'quan_ly_nhan_su'],
 
     # Dữ liệu luôn load
     'data': [
@@ -37,21 +36,8 @@
         'views/bao_tri_views.xml',
         'views/chuyen_giao_views.xml',
         'views/kiem_ke_views.xml',
-        'views/dashboard_views.xml',
-        'views/report_templates.xml',
         'views/menu.xml',
     ],
-
-    # Assets (JS, CSS, XML templates)
-    'assets': {
-        'web.assets_backend': [
-            'quan_ly_tai_san/static/src/css/dashboard.css',
-            'quan_ly_tai_san/static/src/js/dashboard.js',
-        ],
-        'web.assets_qweb': [
-            'quan_ly_tai_san/static/src/xml/dashboard_templates.xml',
-        ],
-    },
 
     'installable': True,
     'application': True,
